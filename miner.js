6a41c8287ff10a4d56eea4f6ce93c61ce74a5053
profek10 <aleksander.grobelnik1@gmail.com>
2023-01-19 22:37:31 +0100

    Reformated code

--- miner.js
+++ miner.js
@@ -1,5 +1,5 @@
-const Block = require("./block");
-const {parentPort, workerData} = require("worker_threads");
+const Block = require('./block')
+const { parentPort, workerData } = require('worker_threads')
 
 const blockChain = []
 const diffAdjustInterval = 5
@@ -11,51 +11,51 @@ let prevHash = 0
 startMining()
 
 function startMining () {
-    let nonce = 0
-    for (let i = 0; index < workerData.data.length; i++) {
-        console.log("Data:")
-        console.log(workerData.data[0])
-        const block = new Block(index, workerData.data[0]["payload"], new Date().getTime(), diff, prevHash, nonce)
-        if (block.hash.substr(0, diff) === '0'.repeat(diff)) {
-            validateBlockchain()
-            blockChain.push(block)
-            parentPort.postMessage(block);
-            index++
-            prevHash = block.hash
-            diff = adjustDifficulty(block)
-            nonce = 0
-        } else {
-            nonce++
-        }
+  let nonce = 0
+  for (let i = 0; index < workerData.data.length; i++) {
+    console.log('Data:')
+    console.log(workerData.data[0])
+    const block = new Block(index, workerData.data[0].payload, new Date().getTime(), diff, prevHash, nonce)
+    if (block.hash.substr(0, diff) === '0'.repeat(diff)) {
+      validateBlockchain()
+      blockChain.push(block)
+      parentPort.postMessage(block)
+      index++
+      prevHash = block.hash
+      diff = adjustDifficulty(block)
+      nonce = 0
+    } else {
+      nonce++
     }
+  }
 }
 
 function validateBlockchain () {
-    const currTS = new Date().getTime()
-    for (let i = 1; i < blockChain.length; i++) {
-        const currBlock = blockChain[i]
-        if (currBlock.validity) { continue }
-        const prevBlock = blockChain[i - 1]
-        if (prevBlock.prevHash === 0 && (currTS - prevBlock.timestamp) <= 60000 && (currTS - currBlock.timestamp) >= 60000) {
-            prevBlock.validity = true
-        }
-        if (currBlock.prevHash === prevBlock.hash && (currTS - prevBlock.timestamp) <= 60000 && (currTS - currBlock.timestamp) >= 60000) {
-            currBlock.validity = true
-        }
+  const currTS = new Date().getTime()
+  for (let i = 1; i < blockChain.length; i++) {
+    const currBlock = blockChain[i]
+    if (currBlock.validity) { continue }
+    const prevBlock = blockChain[i - 1]
+    if (prevBlock.prevHash === 0 && (currTS - prevBlock.timestamp) <= 60000 && (currTS - currBlock.timestamp) >= 60000) {
+      prevBlock.validity = true
     }
+    if (currBlock.prevHash === prevBlock.hash && (currTS - prevBlock.timestamp) <= 60000 && (currTS - currBlock.timestamp) >= 60000) {
+      currBlock.validity = true
+    }
+  }
 }
 
 function adjustDifficulty (block) {
-    if (blockChain.length < diffAdjustInterval) return diffAdjustInterval;
-    const adBlock = blockChain[blockChain.length - diffAdjustInterval]
-    const timeExpected = blockGenerationInterval * diffAdjustInterval * 1000
-    const timeTaken = block.timestamp - adBlock.timestamp
+  if (blockChain.length < diffAdjustInterval) return diffAdjustInterval
+  const adBlock = blockChain[blockChain.length - diffAdjustInterval]
+  const timeExpected = blockGenerationInterval * diffAdjustInterval * 1000
+  const timeTaken = block.timestamp - adBlock.timestamp
 
-    if (timeTaken < (timeExpected / 2)) {
-        return block.diff + 1
-    } else if (timeTaken > (timeExpected * 2)) {
-        return block.diff - 1
-    } else {
-        return block.diff
-    }
+  if (timeTaken < (timeExpected / 2)) {
+    return block.diff + 1
+  } else if (timeTaken > (timeExpected * 2)) {
+    return block.diff - 1
+  } else {
+    return block.diff
+  }
 }
